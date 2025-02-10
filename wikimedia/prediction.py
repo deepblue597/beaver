d2667f6ecff740a8cd4a54f5be0ea1547506909e
deepblue597 <iasonas2001@hotmail.com>
2025-02-11 01:01:42 +0200

    wikipedia events project

--- wikimedia/prediction.py
+++ wikimedia/prediction.py
@@ -1,3 +1,15 @@
+# %%
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
+
+# %%
+# Load the trained model from the file
+with open('HoeffdingTreeClassifier.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# %%
 # New test data
 test_event = {
     "domain": "commons.wikimedia.org",
@@ -15,10 +27,12 @@ y = {"user_type": "human", }  # Target label for testing purposes
 
 # Get the true label (for testing purposes)
 true_label = 1 if y["user_type"] == "bot" else 0  # 1 for bot, 0 for human
-
+# %%
 # Make prediction with the trained model
-predicted_class = model.predict_one(X_test)
+predicted_class = model.predict_one(test_event)
 
 # Print results
 print(f"True label: {'bot' if true_label == 1 else 'human'}")
 print(f"Predicted: {'bot' if predicted_class == 1 else 'human'}")
+
+# %%
