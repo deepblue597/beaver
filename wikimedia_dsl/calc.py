757d012f77c9ffe809f64c4af1a722496697414b
deepblue597 <iasonas2001@hotmail.com>
2025-03-10 15:56:57 +0200

    progress with calculations

--- wikimedia_dsl/calc.py
+++ wikimedia_dsl/calc.py
@@ -5,9 +5,8 @@ evaluation.
 from textx import metamodel_from_str
 
 grammar = '''
-Calc: assignments*=Assignment expression=Final;
+Calc: assignments*=Assignment ;
 Assignment: variable=ID '=' expression=Expression ';';
-Final:  variable=ID '==' expression=Expression ';';
 Expression: operands=Term (operators=PlusOrMinus operands=Term)*;
 PlusOrMinus: '+' | '-';
 Term: operands=Factor (operators=MulOrDiv operands=Factor)*;
@@ -21,11 +20,15 @@ namespace = {}
 
 
 def assignment_action(assignment):
+    # tester.append(assignment.variable)
+    # print('the assignment', assignment.variable)
     namespace[assignment.variable] = assignment.expression
+    tester.append('=')
+    tester.append(assignment.variable)
 
 
-def calc_action(calc):
-    return calc.expression
+# def calc_action(calc):
+#     return calc.expression
 
 
 tester = []
@@ -33,19 +36,24 @@ tester = []
 
 def expression_action(expression):
 
-    ret = expression.operands[0]
-    print(ret)
+    ret = [expression.operands[0]]
+    # print(ret)
+    # tester.append(ret)
+    print(expression.operands)
+    print(expression.operators)
     for operator, operand in zip(expression.operators,
                                  expression.operands[1:]):
-
-        tester.append(operand)
-        tester.append(operator)
-
-        if operator == '+':
-            ret += operand
-        else:
-            ret -= operand
-    print(tester)
+        ret.append(operator)
+        ret.append(operand)
+        # tester.append(operator)
+        # tester.append(operand)
+        # if operator == '+':
+        #     ret += operand
+        # else:
+        #     ret -= operand
+        pass
+    # print(tester)
+    tester.extend(ret)
     return ret
 
 
@@ -54,13 +62,24 @@ def final_action(expression):
 
 
 def term_action(term):
-    ret = term.operands[0]
+    ret = [term.operands[0]]
+    print(term.operands)
+    print(term.operators)
+    # tester.append(ret)
     for operator, operand in zip(term.operators,
                                  term.operands[1:]):
-        if operator == '*':
-            ret *= operand
-        else:
-            ret /= operand
+
+        ret.append(operator)
+        ret.append(operand)
+        # print(operator)
+        # tester.append(operator)
+        # print(operand)
+        # tester.append(operand)
+        # if operator == '*':
+        #     ret *= operand
+        # else:
+        #     ret /= operand
+        pass
     return ret
 
 
@@ -70,28 +89,35 @@ def factor_action(factor):
 
 
 def operand_action(operand):
+
     if operand.op_num is not None:
+        # print('operand is ', operand.op_num)
+        # tester.append(operand.op_num)
         return operand.op_num
     elif operand.op_id:
-        if operand.op_id in namespace:
-            return namespace[operand.op_id]
-        else:
-            raise Exception(f'Unknown variable "{operand.op_id}" '
-                            f'at position {operand._tx_position}')
+        # print('variable is ', operand.op_id)
+        # tester.append(f'sdf[{operand.op_id}]')
+        return operand.op_id
+        # if operand.op_id in namespace:
+        #     return namespace[operand.op_id]
+        # else:
+        #     raise Exception(f'Unknown variable "{operand.op_id}" '
+        #                     f'at position {operand._tx_position}')
     else:
+
         return operand.op_expr
 
 
 def main(debug=False):
 
     processors = {
-        'Calc': calc_action,
+        # 'Calc': calc_action,
         'Assignment': assignment_action,
         'Expression': expression_action,
         'Term': term_action,
         'Factor': factor_action,
         'Operand': operand_action,
-        'Final': final_action
+        # 'Final': final_action
     }
 
     calc_mm = metamodel_from_str(grammar, auto_init_attributes=False,
@@ -99,16 +125,19 @@ def main(debug=False):
     calc_mm.register_obj_processors(processors)
 
     input_expr = '''
-        a = 10;
+        
         b = 2 * a + 17;
-        c == a + b ;
+        c = a + (2 * b +3) ;
     '''
 
     calc = calc_mm.model_from_str(input_expr)
-    result = calc.expression.expression
+    results = calc.assignments
 
     # assert (result - 6.93805555) < 0.0001
-    print("Result is", result)
+    for result in results:
+        print('result is ',  result.variable)
+    print(tester[::-1])
+    # print(namespace)
 
 
 if __name__ == '__main__':
