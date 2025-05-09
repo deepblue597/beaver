cb146379f6450e60f6b1a7e8ff2a613d77bbab1e
deepblue597 <iasonas2001@hotmail.com>
2025-05-09 14:09:51 +0300

    working on model refactoring

--- beaver/refactoring-beaver/gen.py
+++ beaver/refactoring-beaver/gen.py
@@ -12,8 +12,8 @@ def parse_command_line_arguments():
 
     parser.add_argument('--metamodel', default='inheritance.bvr',
                         help='the file in which your pipeline is configured', type=str)
-    # parser.add_argument('--generated_file_name', default='generated_pipeline.py',
-    #                     help='Destination file name', type=str)
+    parser.add_argument('--generated_file_name', default='generated_pipeline.py',
+                        help='Destination file name', type=str)
 
     return parser.parse_args()
 
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     args = parse_command_line_arguments()
 
     # Load the DSL grammar
-    ml_mm = metamodel_from_file('processors.tx')
+    ml_mm = metamodel_from_file('grammar/processors.tx')
 
     # ml_mm.register_obj_processors(processors)
 
@@ -41,17 +41,17 @@ if __name__ == "__main__":
     config = ml_mm.model_from_file(args.metamodel)
 
     # %%
-    # Load Jinja2 template
-    # env = Environment(loader=FileSystemLoader('.'))
-    # template = env.get_template('python.template')
+    #Load Jinja2 template
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('templates/models.jinja')
 
     # flattened_dict = dict_flatten(outpout)
 
-    # generated_code = template.render(
-    #     file=config, assignments=flattened_dict)
+    generated_code = template.render(
+        file=config)
 
-    # # Save the generated code to a file
-    # with open(args.generated_file_name, 'w') as f:
-    #     f.write(generated_code)
+    # Save the generated code to a file
+    with open(args.generated_file_name, 'w') as f:
+        f.write(generated_code)
 
     # print("Generated code saved to:", args.generated_file_name)
