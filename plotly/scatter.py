393fc5e16ef0b9e111645d42d5c8554815d89dc8
deepblue597 <iasonas2001@hotmail.com>
2025-05-11 14:21:58 +0300

    plot tutorial

--- plotly/scatter.py
+++ plotly/scatter.py
@@ -57,3 +57,43 @@ fig.show()
 # import plotly.express as px
 # df = px.data.iris() # iris is a pandas DataFrame
 # df
+
+np.random.seed(42) 
+
+# %%
+random_x = np.random.randint(1 , 101 , 100 ) 
+random_y = np.random.randint(1 , 101 , 100 )
+
+#%% 
+
+#For our case in live data we need to use go.Scatter since px.scatter is not working 
+#No, you cannot directly use plotly.express.scatter (px.scatter) in place of plotly.graph_objects.Scatter (go.Scatter) when working with plotly.subplots.make_subplots. This is because px.scatter returns a complete Figure object, while go.Scatter creates a trace that can be added to a subplot.
+
+# Why?
+# px.scatter: Generates a standalone Figure object, which includes data, layout, and other configurations.
+# go.Scatter: Creates a single trace (data series) that can be added to a Figure or subplot.
+
+
+# marker points instead of lines 
+data = go.Scatter(
+    x = random_x , 
+    y= random_y , 
+    mode = 'markers', 
+    marker=dict(
+        size = 8, 
+        color = 'rgb(51 , 125 , 208 )',
+        symbol = '5', 
+        line = dict(
+            width = 2 
+        )
+    ) 
+)
+
+#add labels , hover mode etc 
+# this cannot be used with live updates because it does not update the layout and causes errors with the values 
+layout = go.Layout(title= 'Random points' , xaxis=dict(title = 'xaxis') , 
+                   yaxis=dict(title = 'y axis'), hovermode='closest')
+
+fig = go.Figure(data = data , layout=layout ) 
+
+fig.show() 
