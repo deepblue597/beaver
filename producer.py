5bebf67d1fda4ce67b6397aebb18aa3132024ae1
deepblue597 <iasonas2001@hotmail.com>
2025-08-22 10:57:55 +0300

    cleaned out code (examples and python tests)

--- producer.py
+++ producer.py
@@ -1,13 +1,8 @@
 # %%
-import json
 import pandas as pd
-from river import datasets, preprocessing
-import kagglehub
+from river import datasets
 import pandas as pd
-from river.datasets import synth
-import random
 from kafka_proj.producer_v2 import parse_command_line_arguments, create_kafka_producer, delivery_callback
-import os
 # %%
 
 def convert_keys_to_underscores(data):
@@ -32,113 +27,30 @@ if __name__ == "__main__":
     producer = create_kafka_producer(
         bootstrap_server=args.bootstrap_server, acks='all',  compression_type='snappy')
 # %%
-    # path = kagglehub.dataset_download("fedesoriano/the-boston-houseprice-data")
-    #dataset = datasets.Phishing()
+    #  For more datasets refer to https://riverml.xyz/latest/api/overview/#datasets_1
+    # Task 1: Phishing dataset 
+    dataset = datasets.Phishing()
+    
+    # Task 3: Trump Approval dataset
     #dataset = datasets.TrumpApproval()
-    #dataset = datasets.ImageSegments()
-    #dataset = datasets.CreditCard()
-    # print("Path to dataset files:", path)
-    #gen = synth.Agrawal(classification_function=0, seed=42)
-    # gen = synth.ConceptDriftStream(stream=synth.SEA(seed=42, variant=0),
-    #                                drift_stream=synth.SEA(seed=42, variant=1),
-    #                                seed=1, position=500, width=50)
-    #dataset = iter(gen.take(1000))
-    # dataset = synth.ConceptDriftStream(
-    #    seed=42,
-    #    position=500,
-    #    width=40).take(1000)
-    #dataset = datasets.Bananas().take(500)
-    # dataset = [
-    #     [1, 2],
-    #     [1, 4],
-    #     [1, 0],
-    #     [-4, 2],
-    #     [-4, 4],
-    #     [-4, 0],
-    #     [5, 0],
-    #     [5, 2],
-    #     [5, 4]
-    # ]
-    #dataset = datasets.AirlinePassengers()
-    
-    #rng = random.Random(12345)
-    #dataset = rng.choices([0, 1], k=1000) + rng.choices(range(4, 8), k=1000)
-    #dataset = synth.Logical(seed=42, n_tiles=100)
-#     dataset = [
-#     ("Chinese Beijing Chinese", "yes"),
-#     ("Chinese Chinese Shanghai", "yes"),
-#     ("Chinese Macao", "yes"),
-#     ("Tokyo Japan Chinese", "no")
-# ]
-    # dataset = (
-    #     ({'user': 'Alice', 'item': 'Superman', 'time': .12}, True),
-    #     ({'user': 'Alice', 'item': 'Terminator', 'time': .13}, True),
-    #     ({'user': 'Alice', 'item': 'Star Wars', 'time': .14}, True),
-    #     ({'user': 'Alice', 'item': 'Notting Hill', 'time': .15}, False),
-    #     ({'user': 'Alice', 'item': 'Harry Potter ', 'time': .16}, True),
-    #     ({'user': 'Bob', 'item': 'Superman', 'time': .13}, True),
-    #     ({'user': 'Bob', 'item': 'Terminator', 'time': .12}, True),
-    #     ({'user': 'Bob', 'item': 'Star Wars', 'time': .16}, True),
-    #     ({'user': 'Bob', 'item': 'Notting Hill', 'time': .10}, False)
-    # )
-    #path = kagglehub.dataset_download("fedesoriano/heart-failure-prediction")
-    #path = kagglehub.dataset_download("unsdsn/world-happiness")
-    #print("Path to dataset files:", path)
-    
-    #dataset = datasets.Elec2()
-    path = kagglehub.dataset_download("vstacknocopyright/electricity")
-# %%
-    path
 
 # %%
-    # csv_path = os.path.join(path, "boston.csv")
-    # Trump approval , Airline , Phising
-    #df = pd.read_csv(dataset.path)
-    #df = pd.read_csv(path+'/2019.csv')
-    df = pd.read_csv(path+'/electricity-normalized.csv')
-    # Bananas, Clustering , CreditCard
-    #df = pd.DataFrame(dataset)
+    # Trump approval , Airline , Phishing
+    df = pd.read_csv(dataset.path)
 # %%
     df
-    
-#%% 
 
-# for idx, row in df.iterrows():
-#     json_message = row.to_json()
-#     #sample_dict = { str(idx): int(row[0])}
-#     #json_message = json.dumps({str(idx): row[0]})
-#     #sample_dict = {**row[0].isoformat(), 'passengers': row[1]}
-#     #sample_dictimport kagglehub
-
-# Download latest version
-#path = kagglehub.dataset_download("fedesoriano/heart-failure-prediction")
-
-#print("Path to dataset files:", path) = {'data': **row[0], 'class': row[1]}
-#     sample_dict = {'data' : json_message}
-#     print(sample_dict)
 # %%
     print('Messages are being published to Kafka topic')
     messages_count = 0
 
     for idx, row in df.iterrows():
 
-        #drift 
-        #sample_dict = { str(idx): int(row[0])}
-        # bananas , CreditCard , facto , Agrawal
-        #sample_dict = {'x' : {**row[0]}, 'class': row[1]}
+        
         # convert to json format
-        #TRump , airline, List dataset 
+        #Trump Approval dataset 
         json_message = row.to_json()
-        #sample_dict = {'data': json.loads(json_message)}
-        #Chinese Beijing
-        #sample_dict = {'value' : row[0], 'class': row[1]}
-        #json_message = convert_keys_to_underscores(json_message)
-        #json_message = json.dumps({str(idx): int(row[0])})
-        #json_message = json.dumps(sample_dict)
-        #print(json_message)
-        #row_dict = row.to_dict()
-        #row_dict_converted = convert_keys_to_underscores(row_dict)
-        #json_message = json.dumps(row_dict_converted)
+
         # Produce the message to kafka
         producer.produce(
             args.topic_name, value=json_message, key=str(idx), callback=delivery_callback)
